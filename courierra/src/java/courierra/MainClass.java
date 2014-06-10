d11609b4be6e043c003ed350cf21609dd4be91ad
eugene-alekseev <eugene-alekseev@users.noreply.github.com>
2014-06-10 16:34:29 +0400

    Update MainClass.java
    checking checking

--- courierra/src/java/courierra/MainClass.java
+++ courierra/src/java/courierra/MainClass.java
@@ -8,7 +8,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello there");
 
 	}
 
